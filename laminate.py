996ab39ee890e52f895bf65540fb239b08cd8bc6
rutgervana <58878404+rutgervana@users.noreply.github.com>
2024-03-30 13:17:56 +0100

    Add more comments and start sequences

--- laminate.py
+++ laminate.py
@@ -7,11 +7,23 @@ from ui.printer import Printer
 def main():
     room = Room(8420, 3795)
     tile = Tile(1380, 244)
-    border = 8
+    border = 10  # space between tiles and wall
     init_tiles = dict()
     n_start_tiles = 3
-    for i in range(n_start_tiles):
-        init_tiles[i] = (i+1) * tile.length/n_start_tiles
+    # most used begining sequences, see what fits your room
+    # # sequence -> whole tile, tile/3
+    # init_tiles[0] = tile.length
+    # init_tiles[1] = tile.length/3
+    # init_tiles[2] = 2*tile.length/3
+    # # sequence -> whole tile, tile/2
+    # init_tiles[0] = tile.length
+    # init_tiles[1] = tile.length/2
+    # # sequence -> 1/n_start_tiles, 2/n_start_tiles, .., whole tile
+    # for i in range(n_start_tiles):
+    #     init_tiles[i] = (i+1) * tile.length/n_start_tiles
+    # # sequence -> whole tile, (n_start_tiles-1)/n_start_tiles, ..., 1/n_start_tiles
+    for i, j in enumerate(reversed(range(n_start_tiles))):
+        init_tiles[i] = (j+1) * tile.length/n_start_tiles
 
     installation = Installation(room, tile, border, init_tiles, reuse_tiles=True)
     plan = installation.process()
